f82f540e8338ab1ee0fa03718f228fdce27937a6
Risovaniy <risovaniysa@gmail.com>
2020-04-09 21:54:01 +0500

    Use new method for print the date

--- main.py
+++ main.py
@@ -3,14 +3,10 @@ from application import salary
 from application.db.people import get_employees
 
 
-def format_date():
-    d = date.today()
-    print(f'{d.day}.{d.month}.{d.year}')
-
-
 if __name__ == '__main__':
     print('Start')
-    format_date()
+    d = date.today().strftime("%d.%m.%Y")
+    print(d)
     new_employee = get_employees('Ivan')
     salery_Ivan = salary.calculate_salary(2500)
     print('Finish')
