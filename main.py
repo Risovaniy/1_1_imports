4f5d4c1f68f2972307d6c851e6d714f5ecbfd7b9
Risovaniy <risovaniysa@gmail.com>
2020-04-09 21:46:08 +0500

    Added a date and move the 'people.py' in 'db'

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+from datetime import date
 from application import salary
-from application.people import get_employees
+from application.db.people import get_employees
+
+
+def format_date():
+    d = date.today()
+    print(f'{d.day}.{d.month}.{d.year}')
 
 
 if __name__ == '__main__':
-    print('Start\n')
+    print('Start')
+    format_date()
     new_employee = get_employees('Ivan')
     salery_Ivan = salary.calculate_salary(2500)
     print('Finish')
